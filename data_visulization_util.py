8ffdaa8b2ce2fc0d0dc32cd485827e2f772d8ab8
Zilin Ma <zm003@bucknell.edu>
2018-08-10 17:05:00 -0700

    utility function

---
+++ data_visulization_util.py
@@ -0,0 +1,103 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import stats
+from scipy.optimize import curve_fit
+
+
+def chisquare(dictionary):
+    contigency_matrix = [[],[]]
+
+    for line in dictionary:
+        acc = 0
+        rej = 0
+        #print(line)
+        for item in dictionary[line]:
+            if item["action"] == "accepted":
+                acc += 1
+            else:
+                rej += 1
+
+        contigency_matrix[0].append(acc)
+        contigency_matrix[1].append(rej)
+    print(contigency_matrix)
+    return stats.chi2_contingency(contigency_matrix)
+
+
+def print_acceptance_rate(dictionary):
+    for line in dictionary:
+        acc_int = 0
+        for item in dictionary[line]:
+            if item['action'] == 'accepted': acc_int += 1
+        print(line)
+        print(acc_int / len(dictionary[line]))
+
+
+def bin_confidence(p, n):
+    print([p, n])
+    return 1.96 * np.sqrt(p*(1- p)/n)
+
+
+def linear(x, a, b):
+    return a + np.multiply(x,b)
+
+
+def plot_dictionary(dictionary, isRegression = False, func = None):
+    acc_rate = dict()
+    p = []
+    n = []
+    for line in sorted(dictionary.keys()):
+        acc_int = 0
+        for item in dictionary[line]:
+            if item['action'] == 'accepted': acc_int += 1
+        acc_rate[line] = (acc_int / len(dictionary[line]))
+        p.append(acc_rate[line])
+        n.append(len(dictionary[line]))
+    p = np.array(p)
+    n = np.array(n)
+    plt.figure()
+    plt.barh(range(len(acc_rate)), list(acc_rate.values()), align='center', xerr=bin_confidence(p, n))
+    plt.yticks(range(len(acc_rate)), list(acc_rate.keys()))
+    plt.show()
+
+    if isRegression and func:
+        dellist = []
+        for i in range(len(p))[::-1]:
+            if p[i] == 0:
+                dellist.append(i)
+        ydata = np.log(p)
+        xdata = np.array(sorted(dictionary.keys()))
+        err =  np.log(bin_confidence(p, n))
+
+
+
+        ydata = np.delete(ydata, dellist)
+        xdata = np.delete(xdata, dellist)
+        err = np.delete(err, dellist)
+
+        params, cov = curve_fit(func, xdata, ydata, sigma = err)
+        residuals = ydata - func(xdata, float(params[0]), float(params[1]))
+        ss_red = np.sum(residuals ** 2)
+        ss_tot = np.sum((ydata - np.mean(ydata)) ** 2)
+        r_sq = 1 - (ss_red / ss_tot)
+        plt.figure()
+        plt.title("log regression")
+        plt.ylabel("log(acceptance rate)")
+        plt.errorbar(xdata, ydata, err)
+        plt.plot(xdata, func(xdata, params[0], params[1]), 'r-', label='fit: a=%5.3f, b=%5.3f' % tuple(params))
+        print('R^2 = %1.3f' % r_sq)
+    return acc_rate
+
+
+def time_period(hour):
+    if 0 <= int(hour) < 6:
+        return 'midnight'
+    if 6 <= int(hour) <= 12:
+        return 'morning'
+    if 12 < int(hour) <= 18:
+        return 'afternoon'
+    if 18 < int(hour) < 24:
+        return 'evening'
+
+
+def select_timestamp(line):
+    return line["timestamp"]
---
+++ efficiencyFunction.py
@@ -0,0 +1,43 @@
+import json
+from collections import Counter
+from data_visulization_util import *
+with open("log_data\\effectiveness.json", "r") as f:
+    effectiveness = json.load(f)
+intervention_to_effectiveness = Counter()
+for line in effectiveness:
+    for intervention in line['intervention_info_list']:
+        intervention_to_effectiveness[intervention[0]] += intervention[2]
+
+with open("log_data\\intervention_to_attrition_rate", "r") as f:
+    intervention_to_attrition_rate_colum = json.load(f)
+intervention_to_attrition_rate = {x.replace(":", "/"):intervention_to_attrition_rate_colum[x] for x in
+                                  intervention_to_attrition_rate_colum}
+
+with open("acc_rate.json", "r") as f:
+    acc_rate = json.load(f)
+
+intervention_to_utility = Counter()
+
+for intervention in intervention_to_attrition_rate:
+    if (intervention in intervention_to_attrition_rate and
+            intervention in acc_rate):
+        try:
+            intervention_to_utility[intervention] = intervention_to_effectiveness.get(intervention) * acc_rate.get(
+                intervention) / \
+                                                    intervention_to_attrition_rate.get(intervention)
+        except ZeroDivisionError:
+            intervention_to_utility[intervention] = -1
+        except TypeError:
+            print(intervention_to_effectiveness.get(intervention))
+            print(acc_rate.get(intervention))
+            print(intervention_to_attrition_rate.get(intervention))
+
+interventions_of_concern = dict()
+for intervention in intervention_to_utility:
+    if intervention.split('/')[0] in ["facebook", "amazon", "youtube", "twitter", "netflix", "reddit", "gmail"]:
+        interventions_of_concern[intervention] = intervention_to_utility[intervention]
+sorted_keys = sorted(interventions_of_concern.keys())
+plt.barh(range(len(interventions_of_concern)), list([interventions_of_concern[x] for x in sorted_keys]), align='center')
+plt.yticks(range(len(interventions_of_concern)), sorted_keys)
+
+plt.show()
