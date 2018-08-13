e8bafebefdd1018f5340f3bc1d878e0eb3124b7a
Zilin Ma <zm003@bucknell.edu>
2018-08-12 23:57:46 -0700

    baseline

--- calculateBaselinePerSession.py
+++ calculateBaselinePerSession.py
@@ -8,9 +8,23 @@ userIDs = set(parse_url_as_json(link))
 domain_to_time_per_day = dict()
 
 idx = 0
+
+link = "http://localhost:5000/get_installs"
+raw = parse_url_as_json(link)
+user_to_install_version = dict()
+for log in raw:
+    try:
+        user_to_install_version[log["user_id"]] = log["version"]
+    except KeyError:
+        print(log)
+
 for user in userIDs:
+
     if idx % 100 == 0: print(str(idx) + '/' + str(len(userIDs)))
     idx += 1
+
+    if user_to_install_version.get(user, "0.0.0") >= "1.0.231": continue
+
     domain_to_time_per_day[user] = Counter()
     link = "http://localhost:5000/printcollection?collection=" + user + "_synced:baseline_time_on_domains"
     raw = parse_url_as_json(link)
